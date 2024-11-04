27353b67044af5941e3a13a00c28447ab8cbf188
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2024-11-04 03:18:03 +0100

    save the output of the checks to a run file and give that to the notifiers

--- files/notify.sh
+++ files/notify.sh
@@ -2,21 +2,22 @@
 
 hostname=$1
 check_id=$2
+run_file=$3
 exit_code=$EXIT_CODE
 
-failed=0;
-pids="";
+failed=0
+pids=""
 for script in /etc/checker/notifiers/*.sh; do
     if [ -x "$script" ]; then
-        echo "Notifying with $script";
-        "$script" "$hostname" "$check_id" "$exit_code" & pids="$pids $!";
-    fi;
-done;
+        echo "Notifying with $script"
+        cat "$run_file" | "$script" "$hostname" "$check_id" "$exit_code" & pids="$pids $!"
+    fi
+done
 for pid in $pids; do
-    wait $pid;
+    wait $pid
     if [ $? -ne 0 ]; then
-        echo "A notifier failed with exit code $?";
-        failed=8;
-    fi;
-done;
-exit $failed;
+        echo "A notifier failed with exit code $?"
+        failed=8
+    fi
+done
+exit $failed
